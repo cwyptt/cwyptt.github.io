d7373f24ab07ff03f36727512962f57d7f075d7c
cwyptt <cwyptt@gmail.com>
2025-01-27 14:01:25 -0600

    Main fix

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import NavHost from '../components/NavHost.svelte';
+	import Hero from '../components/Hero.svelte';
+	import About from '../components/About.svelte';
+	import Repos from '../components/Repos.svelte';
+	import Footer from '../components/Footer.svelte';
+</script>
+
+<NavHost />
+<main>
+	<section id="home">
+		<Hero />
+	</section>
+	<section id="about">
+		<About />
+	</section>
+	<section id="work">
+		<Repos />
+	</section>
+	<Footer />
+</main>
