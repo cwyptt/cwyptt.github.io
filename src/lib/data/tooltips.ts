5148a7525c276d03caf8513aa561b217d5b7bdb4
Alex Hanson <cryptunix@proton.me>
2025-07-08 11:31:58 -0500

    Cleaned up tooltips.ts to be more inclusive

--- src/lib/data/tooltips.ts
+++ src/lib/data/tooltips.ts
@@ -1,5 +1,71 @@
-const tooltipInfo = {
-	certification: {
+// const tooltipInfo = {
+// 	certification: {
+// 		text: 'CompTIA ITF+',
+// 		link: 'https://i.ibb.co/HHM70K5/Alex-Hanson-Comp-TIA-IT-Fundamentals-ITF-Certification-certificate.png',
+// 		tooltip: 'Click for proof of certification'
+// 	},
+// 	cctc: {
+// 		text: 'Caddo Career & Technology Center',
+// 		link: 'https://caddocareer.com/',
+// 		tooltip: 'Click to visit their website'
+// 	},
+// 	preferences: {
+// 		os: {
+// 			text: 'Void Linux',
+// 			tooltip:
+// 				'A rolling release, however stable, Linux distribution utilizing Runit as the init system.',
+// 			link: 'https://www.voidlinux.org'
+// 		},
+// 		wm: {
+// 			text: 'SwayFX',
+// 			tooltip: 'Sway - An i3-compatible Wayland compositor. SwayFX - Sway, but with eye candy.',
+// 			link: 'https://github.com/WillPower3309/swayfx'
+// 		},
+// 		editor: {
+// 			text: 'Spacemacs',
+// 			tooltip: 'Emacs with the Spacemacs distribution for Vim emulation and enhanced productivity.',
+// 			link: 'https://www.spacemacs.org'
+// 		},
+// 		terminal: {
+// 			text: 'Foot',
+// 			tooltip: 'Lightweight terminal emulator for Wayland and uses a client-server architecture.',
+// 			link: 'https://codeberg.org/dnkl/foot'
+// 		}
+// 	}
+// };
+
+// export default tooltipInfo;
+
+export const preferences = {
+	os: {
+		title: 'Operating System',
+		text: 'Void Linux',
+		tooltip:
+			'A rolling release, however stable, Linux distribution utilizing Runit as the init system.',
+		link: 'https://www.voidlinux.org'
+	},
+	wm: {
+		title: 'Window Manager',
+		text: 'SwayFX',
+		tooltip: 'Sway - An i3-compatible Wayland compositor. SwayFX - Sway, but with eye candy.',
+		link: 'https://github.com/WillPower3309/swayfx'
+	},
+	editor: {
+		title: 'Text Editor',
+		text: 'Spacemacs',
+		tooltip: 'Emacs with the Spacemacs distribution for Vim emulation and enhanced productivity.',
+		link: 'https://www.spacemacs.org'
+	},
+	terminal: {
+		title: 'Terminal Emulator',
+		text: 'Foot',
+		tooltip: 'Lightweight terminal emulator for Wayland that uses a client-server architecture.',
+		link: 'https://codeberg.org/dnkl/foot'
+	}
+};
+
+export const originalTooltips = {
+	comptiaitf: {
 		text: 'CompTIA ITF+',
 		link: 'https://i.ibb.co/HHM70K5/Alex-Hanson-Comp-TIA-IT-Fundamentals-ITF-Certification-certificate.png',
 		tooltip: 'Click for proof of certification'
@@ -8,30 +74,5 @@ const tooltipInfo = {
 		text: 'Caddo Career & Technology Center',
 		link: 'https://caddocareer.com/',
 		tooltip: 'Click to visit their website'
-	},
-	preferences: {
-		os: {
-			text: 'Void Linux',
-			tooltip:
-				'A rolling release, however stable, Linux distribution utilizing Runit as the init system.',
-			link: 'https://www.voidlinux.org'
-		},
-		wm: {
-			text: 'SwayFX',
-			tooltip: 'Sway - An i3-compatible Wayland compositor. SwayFX - Sway, but with eye candy.',
-			link: 'https://github.com/WillPower3309/swayfx'
-		},
-		editor: {
-			text: 'Spacemacs',
-			tooltip: 'Emacs with the Spacemacs distribution for Vim emulation and enhanced productivity.',
-			link: 'https://www.spacemacs.org'
-		},
-		terminal: {
-			text: 'Foot',
-			tooltip: 'Lightweight terminal emulator for Wayland and uses a client-server architecture.',
-			link: 'https://codeberg.org/dnkl/foot'
-		}
 	}
 };
-
-export default tooltipInfo;
