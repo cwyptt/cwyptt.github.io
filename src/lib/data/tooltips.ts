f58cae3166ee56991868d40d079339caf097501d
Alex Hanson <cryptunix@proton.me>
2025-07-07 20:20:42 -0500

    Add tooltips data file

---
+++ src/lib/data/tooltips.ts
@@ -0,0 +1,14 @@
+const tooltipInfo = {
+	certification: {
+		text: 'CompTIA ITF+',
+		link: 'https://i.ibb.co/HHM70K5/Alex-Hanson-Comp-TIA-IT-Fundamentals-ITF-Certification-certificate.png',
+		tooltip: 'Click for proof of certification'
+	},
+	cctc: {
+		text: 'Caddo Career & Technology Center',
+		link: 'https://caddocareer.com/',
+		tooltip: 'Click to visit their website'
+	}
+};
+
+export default tooltipInfo;
